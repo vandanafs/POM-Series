6dd21e2d2956045ea7ef3ed8bfd276726c6d0a0f
vandanafs <vandana.f.09@gmail.com>
2020-03-28 21:33:33 -0700

    DealsPage added

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,5 +1,13 @@
 package com.qa.hubspot.pages;
 
-public class DealsPage {
+import org.openqa.selenium.WebDriver;
 
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage {
+	WebDriver driver;
+
+	public DealsPage(WebDriver driver) {
+	  this.driver=driver;
+	  }
 }
