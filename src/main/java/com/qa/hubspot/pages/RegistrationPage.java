803cfb649f84793638952d8b9d35103ad76860e6
vandanafs <60112122+vandanafs@users.noreply.github.com>
2020-03-29 21:00:37 -0700

    Update RegistrationPage.java
    updated reg page class

--- src/main/java/com/qa/hubspot/pages/RegistrationPage.java
+++ src/main/java/com/qa/hubspot/pages/RegistrationPage.java
@@ -3,5 +3,6 @@ package com.qa.hubspot.pages;
 public class RegistrationPage {
   public RegistrationPage() {
 	  System.out.println("Registration page is created ");
+	  System.out.println("Registration page is changed at github ");
   }
 }
