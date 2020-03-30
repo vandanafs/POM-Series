80f02109061cdc82fa466d57ddbda59d43c55ea8
vandanafs <60112122+vandanafs@users.noreply.github.com>
2020-03-29 21:18:12 -0700

    Create TicketsPage.java
    added TicketsPage

---
+++ src/main/java/com/qa/hubspot/pages/TicketsPage.java
@@ -0,0 +1,14 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class TicketsPage  extends BasePage{
+	WebDriver driver;
+	
+	
+	public TicketsPage(WebDriver driver) {
+		this.driver=driver;
+	}
+}
